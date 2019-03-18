05a24e22a60667d5510fbd9e802a7c6edeff0fcb
zongjiehao <594796355@qq.com>
2019-03-18 17:14:29 +0800

    orm

--- www/orm.py
+++ www/orm.py
@@ -156,11 +156,84 @@ class Model(dict,metaclass=ModelMetaclass):
     def __setattr__(self, key, value):
         self[key] = value
 
-    def get_value(self,key):
+    def getValue(self,key):
         return getattr(self,key,None)
 
+    def getValueOrDefault(self,key):
+        value = getattr(self,key,None)
+        if value is None:
+            field = self.__mappings__[key]
+            if field.default is not None:
+                value = field.default() if callable(field.default) else field.default
+                setattr(self,key,value)
+                logging.debug('using default value for %s: %s' % (key, str(value)))
+        return value
+    #根据where条件查找
+    @classmethod
+    async def findAll(cls,where=None,args=None,**kw):
+        sql = [cls.__select__]
+        if where:
+            sql.append('where')
+            sql.append(where)
+        if args is None:
+            args=[]
+        orderBy = kw.get('orderBy',None)
+        if orderBy:
+            sql.append('order by')
+            sql.append(orderBy)
+        limit = kw.get('limit',None)
+        if limit is not None:
+            sql.append('limit')
+            if isinstance(limit,int):
+                sql.append(limit)
+                sql.append('?')
+                args.append(limit)
+            elif isinstance(limit,tuple) and len(limit) == 2:
+                sql.append('?,?')
+                args.extend(limit)
+            else:
+                raise ValueError('Invalid limit value: %s' % str(limit))
+        rs = await select(' '.join(sql),args,1)
+        # **r 是关键字参数，构成了一个cls类的列表，其实就是每一条记录对应的类实例
+        return [cls(**r) for r in rs]
 
+    @classmethod
+    #findNumber() - 根据WHERE条件查找，但返回的是整数，适用于select count(*)类型的SQL。
+    async def findNumber(cls,selectField,where=None,args=None):
+        sql = ['select %s _num_ from `%s`' %(selectField,cls.__table__)]
+        if where:
+            sql.append('where')
+            sql.append(where)
+        rs = await select(' '.join(sql),args,1)
+        if len(rs)==0:
+            return None
+        return rs[0]['num']
 
+    @classmethod
+    async def find(cls,pk):
+        rs = await select('%s where `%s`=?' %(cls.__select__, cls.__primary_key__), [pk], 1)
+        if len(rs)==0:
+            return None
+        return cls(**rs[0])
 
+    async def save(self):
+        args=list(map(self.getValueOrDefault,self.__fields__))
+        args.append(self.getValueOrDefault(self.__primary_key__))
+        rows=await execute(self.__update__,args)
+        if rows != 1:
+            logging.warning('failed to insert record: affected rows: %s' % rows)
+
+    async def update(self):
+        args = list(map(self.getValue,self.__fields__))
+        args.append(self.getValue(self.__primary_key__))
+        rows = await execute(self.__update__,args)
+        if rows != 1:
+            logging.warning('failed to update by primary key: affected rows: %s' % rows)
+
+    async def remove(self):
+        args = [self.getValue(self.__primary_key__)]
+        rows = await execute(self.__delete__,args)
+        if rows != 1:
+            logging.warning('failed to remove by primary key: affected rows: %s' % rows)
 
 
